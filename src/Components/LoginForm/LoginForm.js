22b5acb7a61b67d1124349994425279e53e3cfc5
Joel <joelmkim382001@gmail.com>
2025-02-06 22:09:21 -0500

    Add some className components to style for CSS

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -6,28 +6,29 @@ import { FaEye } from "react-icons/fa";
 const LoginForm = () => {
   return (
     <div>
-      <form action="">
-        <h1>Login</h1>
-        <div className="input-box">
-          <input type="text" placeholder='Username' required/>
-          <FaUser />
+      <div className="wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder='Username' required/>
+            <FaUser className="icon"/>
+          </div>
+          <div className="input-box">
+            <input type="text" placeholder='Password' required/>
+            <FaEye className="icon"/>
+          </div>
+
+        </form>
+        <div className="remember-forgot">
+          <label><input type="checkbox" /><span></span>Remember Me</label>
+          <a href="#">Forgot Password</a>
         </div>
-        <div className="input-box">
-          <input type="text" placeholder='Password' required/>
-          <FaEye />
+        <button className = "login-button" type="submit">Login</button>
+
+        <div className="sign-up">
+          <p>Don't have an account? <a href="#">Sign Up</a></p>
         </div>
-
-      </form>
-      <div className="remember-forgot">
-        <label><input type="checkbox" />Remember Me</label>
-        <a href="#">Forgot Password</a>
       </div>
-      <button type="submit">Login</button>
-
-      <div className="sign-up">
-        <p>Don't have an account? <a href="#">Sign Up</a></p>
-      </div>
-
     </div>
   )
 }
